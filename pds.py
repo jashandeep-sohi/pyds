042fa06df97eec8d2316aad45d5b72fbcf55fd01
Jashandeep Sohi <jashandeep.s.sohi@gmail.com>
2014-05-07 01:06:52 -0700

    Initalized files.

---
+++ pds.py
@@ -0,0 +1,28 @@
+# pds
+# Copyright (C) 2014 Jashandeep Sohi
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+class PDS(object):
+	pass
+
+class PDSFile(object):
+	pass
+	
+def parse(string):
+	pass
+	
+def open(filename):
+	pass
+# vim: tabstop=2 noexpandtab
---
+++ setup.py
@@ -0,0 +1,30 @@
+# pds
+# Copyright (C) 2014 Jashandeep Sohi
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from distutils.core import setup
+
+if __name__ == "__main__":
+ setup(
+  name = "pds",
+  version = "0.1",
+  description = "Python module to parse PDS labels.",
+  author = "Jashandeep Sohi",
+  author_email = "jashandeep.s.sohi@gmail.com",
+  url = "https://github.com/jashandeep-sohi/pds",
+  license = "GPLv3",
+  py_modules = ["pds"]
+ )
+# vim: tabstop=2 noexpandtab
