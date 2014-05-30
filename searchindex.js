9495c369a726c5dcca330425659a5e185cb6ce55
Jashandeep Sohi <jashandeep.s.sohi@gmail.com>
2014-05-30 15:31:19 -0700

    Updated documentation

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({objects:{},titles:["Welcome to pds&#8217;s documentation!"],objnames:{},objtypes:{},envversion:43,filenames:["index"],terms:{search:0,page:0,modul:0,index:0,content:0},titleterms:{document:0,tabl:0,indic:0,welcom:0}})
+Search.setIndex({envversion:43,objtypes:{},titleterms:{welcom:0,document:0,indic:0,tabl:0},titles:["Welcome to pds&#8217;s documentation!"],objects:{},terms:{index:0,page:0,content:0,search:0,modul:0},objnames:{},filenames:["index"]})
