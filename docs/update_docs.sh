468c6547b70755ed48ccabfd1f5277cd9454d0b7
Jashandeep Sohi <jashandeep.s.sohi@gmail.com>
2014-05-30 15:13:58 -0700

    Updated documentation

--- docs/update_docs.sh
+++ docs/update_docs.sh
@@ -3,5 +3,11 @@
 git add -A . &&
 git commit -m "Updated documentation"
 
-make html
-
+make html &&
+git checkout gh-pages &&
+git rm -r :/ &&
+git reset -- :/CNAME :/.nojekyll && git checkout -- :/CNAME :/.nojekyll &&
+mv ./_build/html/* ../. &&
+git add -A :/ &&
+git commit -m "Updated documentation" &&
+git checkout master
