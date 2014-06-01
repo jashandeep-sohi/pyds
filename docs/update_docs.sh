2fe3cdfe95b0774082146ac44befd0b6d3c94fcf
Jashandeep Sohi <jashandeep.s.sohi@gmail.com>
2014-06-01 15:48:29 -0700

    Updated documentation

--- docs/update_docs.sh
+++ docs/update_docs.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-sphinx-build -W -b doctest -b html . _build/html &&
-git add -A . &&
-git commit -m "Updated documentation" &&
-git push origin master
-
+sphinx-build -W -b doctest . _build/doctest &&
+sphinx-build -W -b html . _build/html &&
 git stash &&
 git checkout gh-pages &&
 git rm -r :/ &&
@@ -16,4 +13,7 @@ git reset -- :/docs &&
 git commit -m "Updated documentation" &&
 git push origin gh-pages &&
 git checkout master &&
-git stash pop --index
+git stash pop --index &&
+git add -A . &&
+git commit -m "Updated documentation" &&
+git push origin master
